a3584f355f421babb5dc2b62177072a659542efc
IT18039078 <IT18039078@my.sliit.lk>
2022-07-26 22:18:00 +0530

    The problem was solved - by checking all possible test cases

--- easy/Longest_Common_Prefix.py
+++ easy/Longest_Common_Prefix.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: str
         """
         biggest_prefix = strs[0]
+        counter = len(biggest_prefix)
         list_length = len(strs)
         # first have to findout the which string is smaller one - 
         # because that have the possiblity to be biggest prefix
@@ -21,23 +22,23 @@ class Solution(object):
                         string_length = len(strs[i])
                         if(( string_length >= 0 and string_length <= 200) and strs[i].islower()):
                             # finout the longest common string in the list
-                            for i in range(len(strs)):
-                                string = strs[i]
-                                if(string.startswith(biggest_prefix)):
-                                    biggest_prefix = biggest_prefix
-                                else:
-                                    biggest_prefix = biggest_prefix[:-1]                                
+                            for i in range(counter):
+                                for j in range(len(strs)):
+                                    if(strs[j].startswith(biggest_prefix)):
+                                        biggest_prefix = biggest_prefix
+                                    else:
+                                        biggest_prefix = biggest_prefix[:-1]                                
                                 
                         else:
                             if(strs[i] == ''):
-                                biggest_prefix = ' '
+                                biggest_prefix = ""
         
 
         
         if(len(biggest_prefix) ==  1 and biggest_prefix == " "):
-            biggest_prefix = ' '
+            biggest_prefix = ""
         elif(biggest_prefix == ""):
-            biggest_prefix = ' '
+            biggest_prefix = ""
 
         return biggest_prefix
 
