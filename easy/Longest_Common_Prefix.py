71fc8af5d3876e25ac41185cf3b15a7f86aee504
IT18039078 <IT18039078@my.sliit.lk>
2022-07-26 21:57:46 +0530

    The logic was found some of the validation should cover to finish the solution

--- easy/Longest_Common_Prefix.py
+++ easy/Longest_Common_Prefix.py
@@ -1,48 +1,45 @@
-from asyncio.windows_events import NULL
-
-
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
         :rtype: str
         """
-        biggest_prefix = ''
-        stop = 1
-        length = 0
+        biggest_prefix = strs[0]
         list_length = len(strs)
         # first have to findout the which string is smaller one - 
         # because that have the possiblity to be biggest prefix
-        if(list_length != 0):
-            if(len(strs[0]) < len(strs[1])):
-                if(len(strs[0]) < len(strs[2])):
-                    biggest_prefix =  strs[0] 
-            elif(len(strs[1]) < len(strs[2])):
-                biggest_prefix = strs[1]
-            else:
-                biggest_prefix = strs[2]
+        if(list_length > 1):
+                for i in range(len(strs)):
+                    if(len(strs[i]) < len(biggest_prefix)):
+                        biggest_prefix = strs[i]
 
-        length = len(biggest_prefix)
-        string_length = 0
-        # code for the contraints
-        if(  (len(strs) <= 200 and len(strs) >= 1)):
-            for i in range(len(strs)):
-                string_length = len(strs[i])
-                if(( string_length >= 0 and string_length <= 200) and strs[i].islower()):
-                    # finout the longest common string in the list
-                    while(stop == 1 and length > 0):
-                        if(biggest_prefix in strs[0] and biggest_prefix in strs[1] and biggest_prefix in strs[2]):
-                            length = 0
-                            stop = 0 
-                            return biggest_prefix           
+                length = len(biggest_prefix)
+                string_length = 0
+                # code for the contraints
+                if(  (len(strs) <= 200 and len(strs) >= 1)):
+                    for i in range(len(strs)):
+                        string_length = len(strs[i])
+                        if(( string_length >= 0 and string_length <= 200) and strs[i].islower()):
+                            # finout the longest common string in the list
+                            for i in range(len(strs)):
+                                string = strs[i]
+                                if(string.startswith(biggest_prefix)):
+                                    biggest_prefix = biggest_prefix
+                                else:
+                                    biggest_prefix = biggest_prefix[:-1]                                
+                                
                         else:
-                            biggest_prefix = biggest_prefix[:-1]
-                            length = len(strs) - 1
-        else:
-            return "string is not  good"
-        if(len(biggest_prefix) == 0):
-            return ""
-        else:
-            return biggest_prefix
-strs = ["Flower","flow","flight"]
+                            if(strs[i] == ''):
+                                biggest_prefix = ' '
+        
+
+        
+        if(len(biggest_prefix) ==  1 and biggest_prefix == " "):
+            biggest_prefix = ' '
+        elif(biggest_prefix == ""):
+            biggest_prefix = ' '
+
+        return biggest_prefix
+
+strs = ["flower","fkow"]
 print(Solution.longestCommonPrefix(Solution, strs))
