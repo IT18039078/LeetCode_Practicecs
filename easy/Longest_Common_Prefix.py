931484e5c535da216058abcd51e4823a3729b8f2
IT18039078 <IT18039078@my.sliit.lk>
2022-07-26 14:15:14 +0530

    Added new problem to sole, that is find out the liongest common prefix in the string list 1. Initial was found out the possible biggest prefix by assigning the smallest string the list 2. put all the validation to the problem 3. working fine for normally for 3 string in the list 4. but other validation like empty one string, and one string is not working
    have to find the way to fix these validation

--- easy/Longest_Common_Prefix.py
+++ easy/Longest_Common_Prefix.py
@@ -1,21 +1,48 @@
+from asyncio.windows_events import NULL
+
+
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
         :rtype: str
         """
-        biggest_prefix   = ''
+        biggest_prefix = ''
+        stop = 1
+        length = 0
+        list_length = len(strs)
         # first have to findout the which string is smaller one - 
         # because that have the possiblity to be biggest prefix
-        if(len(strs[0]) < len(strs[1])):
-            if(len(strs[0]) < len(strs[2])):
-                biggest_prefix =  strs[0] 
-        elif(len(strs[1]) < len(strs[2])):
-            biggest_prefix = strs[1]
-        else:
-            biggest_prefix = strs[2]
+        if(list_length != 0):
+            if(len(strs[0]) < len(strs[1])):
+                if(len(strs[0]) < len(strs[2])):
+                    biggest_prefix =  strs[0] 
+            elif(len(strs[1]) < len(strs[2])):
+                biggest_prefix = strs[1]
+            else:
+                biggest_prefix = strs[2]
 
-        
-        return biggest_prefix
-strs = ["flower","flow","flight"]
+        length = len(biggest_prefix)
+        string_length = 0
+        # code for the contraints
+        if(  (len(strs) <= 200 and len(strs) >= 1)):
+            for i in range(len(strs)):
+                string_length = len(strs[i])
+                if(( string_length >= 0 and string_length <= 200) and strs[i].islower()):
+                    # finout the longest common string in the list
+                    while(stop == 1 and length > 0):
+                        if(biggest_prefix in strs[0] and biggest_prefix in strs[1] and biggest_prefix in strs[2]):
+                            length = 0
+                            stop = 0 
+                            return biggest_prefix           
+                        else:
+                            biggest_prefix = biggest_prefix[:-1]
+                            length = len(strs) - 1
+        else:
+            return "string is not  good"
+        if(len(biggest_prefix) == 0):
+            return ""
+        else:
+            return biggest_prefix
+strs = ["Flower","flow","flight"]
 print(Solution.longestCommonPrefix(Solution, strs))
